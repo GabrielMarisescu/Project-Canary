c0012a4c6b71aed42b89f8fcaaed8cd0af53d45f
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-02-20 22:42:09 +0100

    Palette API creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState<any>([]);
+
+  const fetchUrl = 'http://colormind.io/api/';
+  const fetchData: any = {
+    model: 'Default',
+  };
+
+  const headers = {
+    'Content-Type': 'text/plain',
+  };
+
+  useEffect(() => {
+    axios
+      .post(fetchUrl, fetchData, { headers })
+      .then((res) => console.log(res.data));
+  }, [data]);
+
   return (
     <>
       <div className='text-7xl font-bold underline'>Hello world!</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@ module.exports = {
   mode: 'jit',
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      color1: '#1fb6ff',
+      color2: '#7e5bef',
+      color3: '#ff49db',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   plugins: [],
 };
