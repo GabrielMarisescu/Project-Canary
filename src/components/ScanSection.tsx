6f79784906f9e1d7c3ff905386207491887bb002
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-03-04 14:37:58 +0100

    fixed layout ScanSection

--- src/components/ScanSection.tsx
+++ src/components/ScanSection.tsx
@@ -1,57 +1,56 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { ScanSectionProps } from '../interfaces';
-import { Paper, Typography } from '@material-ui/core';
+import { Paper, TextField, Typography } from '@material-ui/core';
 import logoMain from '../assets/Logogab.png';
+import { Search } from '@mui/icons-material';
 
 function ScanSection({ result }: ScanSectionProps): JSX.Element {
   const [analysisResult, setAnalysisResult] = useState<any>();
   const [inputURL, setInputUrl] = useState<string>();
   const [url, setUrl] = useState<any>();
   const virusTotalApiKey = process.env.REACT_APP_API_KEY;
+  const analysisData = url?.data?.id;
 
-  const submitData = (e: { preventDefault: () => void }) => {
-    analyze();
-    e.preventDefault();
-    setInputUrl('');
+  const optionsEncoder = {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'x-apikey': `${virusTotalApiKey}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      url: `${inputURL}`,
+    }),
   };
 
+  const optionsAnalysis = {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'x-apikey': `${virusTotalApiKey}`,
+    },
+  };
+
+  const submitData = useCallback((e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    //  setInputUrl('');
+  }, []);
+
+  useEffect(() => {}, [submitData]);
+
   const analyze = () => {
-    const analysisData = url?.data?.id;
-    const optionsEncoder = {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'x-apikey': `${virusTotalApiKey}`,
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: new URLSearchParams({
-        url: `${inputURL}`,
-      }),
-    };
-    const optionsAnalysis = {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'x-apikey': `${virusTotalApiKey}`,
-      },
-    };
+    fetch('https://www.virustotal.com/api/v3/urls', optionsEncoder)
+      .then((response) => response.json())
+      .then((response) => setUrl(response))
+      .catch((err) => console.error(err));
 
-    if (inputURL) {
-      fetch('https://www.virustotal.com/api/v3/urls', optionsEncoder)
-        .then((response) => response.json())
-        .then((response) => setUrl(response))
-        .catch((err) => console.error(err));
-    }
-
-    if (analysisData) {
-      fetch(
-        'https://www.virustotal.com/api/v3/analyses/' + analysisData,
-        optionsAnalysis
-      )
-        .then((response) => response.json())
-        .then((response) => setAnalysisResult(response))
-        .catch((err) => console.error(err));
-    }
+    fetch(
+      'https://www.virustotal.com/api/v3/analyses/' + analysisData,
+      optionsAnalysis
+    )
+      .then((response) => response.json())
+      .then((response) => setAnalysisResult(response))
+      .catch((err) => console.error(err));
   };
 
   console.log(url?.data?.id, result, analysisResult);
@@ -68,20 +67,34 @@ function ScanSection({ result }: ScanSectionProps): JSX.Element {
         </div>
       </Paper>
 
-      <Typography variant='h5' className='flex important justify-center  mb-10'>
-        Analyze suspicious URLs to detect malware.
-      </Typography>
-
-      <form className='border-2 border-indigo-100 mt-20 w-screen md:w-1/2 inline-flex'>
-        <input
-          type='text'
-          name='value'
-          onChange={(e) => setInputUrl(e.target.value)}
-          placeholder='Write a link here'
-          className='w-full'
-        />
-        <button type='submit' onClick={submitData} className='hidden' />
-      </form>
+      <div className=' mb-2 ml-4 mr-4'>
+        <Typography
+          variant='h5'
+          className='flex important justify-center  md:text-lg antialiased font-normal'
+        >
+          Analyze suspicious URLs to detect malware
+        </Typography>
+      </div>
+      <div className='flex justify-center ml-4 mr-4'>
+        <form className='mt-20 w-screen md:w-1/2 p-1 ml-2 border-none outline-none flex justify-center'>
+          <TextField
+            autoComplete='off'
+            type='text'
+            name='value'
+            onChange={(e) => setInputUrl(e.target.value)}
+            placeholder='Scan an URL'
+            className='w-full'
+          />
+          <Search
+            type='submit'
+            onClick={submitData}
+            className=' ml-2 mt-2 active:border-indigo-300'
+          />
+        </form>
+      </div>
+      <div className='flex justify-center'>
+        {analysisResult ? analysisResult.data.attributes.status : null}
+      </div>
     </>
   );
 }
