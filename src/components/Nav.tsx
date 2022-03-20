92f503754e844340ed303fba2ae475cfc9686675
Jorge Gonzalez <jglez.cs@gmail.com>
2022-03-20 16:31:31 -0500

    Rewrites CSS to Tailwind

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,17 +6,19 @@ import hamburger from "../assets/menu.png";
 function Nav() {
   return(
     <>
-      <nav>
-        <a id="home-link">
-          <img id="headshot" src={gabe} />
+      <nav className="tracking-widest flex justify-between h-24 text-lg uppercase text-zinc-500 font-serif">
+
+        <a className="flex items-center hover:text-amber-500 ease-in duration-500 ml-20">
+          <img className="rounded-full mr-4 h-14" src={gabe} />
           <p>gabriel marisescu</p>
         </a>
 
-        <div className="links">
-          <a>About</a>
-          <a>Projects</a>
-          <a>Contact</a>
+        <div className="flex items-center">
+          <a className="mr-20 hover:text-amber-500 ease-in duration-500">About</a>
+          <a className="mr-20 hover:text-amber-500 ease-in duration-500">Projects</a>
+          <a className="mr-20 hover:text-amber-500 ease-in duration-500">Contact</a>
         </div>
+        
       </nav>
     </>
   )
