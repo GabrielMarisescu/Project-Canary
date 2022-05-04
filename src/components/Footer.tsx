130f1eac7052c55e11e54f4b218d5baa1a148ec5
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-05-04 15:11:08 +0200

    added margin footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import githubLogo from '../assets/githubLogo.svg';
 
 function Footer() {
   return (
-    <div className='overflow-hidden text-zinc-800 antialiased invisible  md:visible h-52 bg-indigo-100 lg:h-64 '>
+    <div className='overflow-hidden text-zinc-800 antialiased invisible mt-10  md:visible h-52 bg-indigo-100 lg:h-64 '>
       <footer className='flex w-screen h-36 mt-5 bg-indigo-100'>
         <div className=' flex-grow-4 flex'>
           <div className='flex flex-col mx-12 justify-center mb-10 text-center'>
