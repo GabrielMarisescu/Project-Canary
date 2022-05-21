d6d60d4693edc57c929566fde5cdb9bcb04ad69a
Gabriel Marisescu <gabriel.lavoro2@gmail.com>
2022-05-21 13:37:56 +0200

    footer &header aria labels and types

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,20 @@
 import linkedinLogo from '../assets/linkedInLogo.svg';
 import githubLogo from '../assets/githubLogo.svg';
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
 
-function Footer() {
+function Footer(): EmotionJSX.Element {
   return (
     <div className='overflow-hidden text-zinc-800 antialiased invisible mt-10  md:visible h-52 bg-indigo-100 lg:h-64 '>
-      <footer className='flex w-screen h-36 mt-5 bg-indigo-100'>
+      <footer
+        className='flex w-screen h-36 mt-5 bg-indigo-100'
+        aria-label='footer'
+      >
         <div className=' flex-grow-4 flex'>
           <div className='flex flex-col mx-12 justify-center mb-10 text-center'>
-            <div className='prose-lg  mx-auto font-bold mt-5'>
+            <div className='prose-lg  mx-auto font-bold mt-5' lang='en'>
               Project Canary
             </div>
-            <p className='prose-md text-md text-center'>
+            <p className='prose-md text-md text-center' lang='en'>
               Project Canary uses the VirusTotal API and is actively mantained
               by developers.
             </p>
@@ -18,17 +22,23 @@ function Footer() {
         </div>
         <div className=' flex-grow-2 flex justify-center content-center flex-col'>
           <div className='flex'>
-            <a href='https://github.com/GabrielMarisescu/Project-Canary'>
+            <a
+              href='https://github.com/GabrielMarisescu/Project-Canary'
+              aria-label='GitHub Link'
+            >
               <img
                 className=' object-contain h-16 w-32 mt-4 cursor-pointer'
-                alt='github'
+                alt='GitHub Logo'
                 src={githubLogo}
               />
             </a>
-            <a href='https://www.linkedin.com/in/gabriel-marisescu/'>
+            <a
+              href='https://www.linkedin.com/in/gabriel-marisescu/'
+              aria-label='linkedIn Link'
+            >
               <img
                 className=' object-contain h-16 w-32 mt-4 cursor-pointer'
-                alt='linkedin'
+                alt='linkedIn Logo'
                 src={linkedinLogo}
               />
             </a>
@@ -38,7 +48,7 @@ function Footer() {
       <div className=' flex bg-indigo-100'>
         <hr className='border-gray-400 border-t-1  w-4/5  m-auto mb-2 ' />
       </div>
-      <p className='prose-xl  flex justify-center bg-indigo-100'>
+      <p className='prose-xl  flex justify-center bg-indigo-100' lang='en'>
         © 2022 Gabriel Marisescu. All rights reserved.
       </p>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import { Avatar } from '@mui/material';
 import mainProfile from '../assets/Canary.png';
 import { Link } from 'react-router-dom';
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
 
-function Header() {
+function Header(): EmotionJSX.Element {
   return (
     <>
       <nav className=' flex justify-center h-24 text-lg text-zinc-800 prose-p:font-bold '>
@@ -13,7 +14,9 @@ function Header() {
               src={mainProfile}
               sx={{ width: 56, height: 56 }}
             />
-            <p className=' flex self-center ml-5'>Project Canary</p>
+            <p className=' flex self-center ml-5' lang='en'>
+              Project Canary
+            </p>
           </div>
         </Link>
         <div className=' mt-14 mr-5'></div>
